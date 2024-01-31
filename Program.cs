b465decddcbf4fcccda06b1e6e9dbf3cef677fd4
alexfankhauser <alexfankhauser@ALEXFANKHAU8EDB>
2024-01-31 10:52:30 -0700

    do loop

--- Program.cs
+++ Program.cs
@@ -13,34 +13,44 @@ int choice;
 int start = 0;
 // give the players their symbols 
 Console.WriteLine("PLayer1:X and Player2:O");
-// show them who's turn it is
-if (Player % 2 == 0)
+do
 {
-    Console.WriteLine("Player 2's turn!");
-}
-else
-{
-    Console.WriteLine("Player 1's turn!");
-}
-// print the Board for the player
-tk.PrintBoard();
-// read line to see what the user wants to do int.parse initializing
-Console.WriteLine("Where do you want to put your mark?");
-// gather the users choice
-choice = int.Parse(Console.ReadLine());
-// if else statement to check the place in the array
-if (PlayerArray[choice] != 'X' && PlayerArray[choice] != 'O') ;
-{
+    // show them who's turn it is
     if (Player % 2 == 0)
     {
-        PlayerArray[choice] = 'O';
-        Player++;
+        Console.WriteLine("Player 2's turn!");
     }
     else
     {
-        PlayerArray[choice] = 'X';
-        Player++;
+        Console.WriteLine("Player 1's turn!");
     }
-}
-tk.CheckWinner(PlayerArray);
+    // print the Board for the player
+    tk.PrintBoard();
+    // read line to see what the user wants to do int.parse initializing
+    Console.WriteLine("Where do you want to put your mark?");
+    // gather the users choice
+    choice = int.Parse(Console.ReadLine());
+    // if else statement to check the place in the array
+    if (PlayerArray[choice] != 'X' && PlayerArray[choice] != 'O') ;
+    {
+        if (Player % 2 == 0)
+        {
+            PlayerArray[choice] = 'O';
+            Player++;
+        }
+        else
+        {
+            PlayerArray[choice] = 'X';
+            Player++;
+        }
+    }
+    result = tk.CheckWinner(PlayerArray);
+
+    if (result)
+    {
+        start = 1;
+    }
+
+} while (start == 0);
 // print who the winner is and end the game
+Console.WriteLine($"Player {result[2]} is the winner!");
