a3e564742ab38ab4144d1b7983695c0783a892ee
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-01-31 10:46:33 -0700

    commentingandfinalizing

--- Program.cs
+++ Program.cs
@@ -5,26 +5,28 @@ using System.Runtime.CompilerServices;
 using Mission04___Team1_11;
 
 TikTakBoard tk = new TikTakBoard();
-char[] PlayerArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+// make an array that stores player's Choices; if they choose a spot then it will store either an X or O
+char[] PlayerArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+// start with Player equaling 1
 int Player = 1;
 int choice;
 int start = 0;
+// give the players their symbols 
 Console.WriteLine("PLayer1:X and Player2:O");
+// show them who's turn it is
 if (Player % 2 == 0)
 {
-<<<<<<< HEAD
-    // testing
-}
-=======
     Console.WriteLine("Player 2's turn!");
 }
 else
 {
     Console.WriteLine("Player 1's turn!");
 }
+// print the Board for the player
 tk.PrintBoard();
 // read line to see what the user wants to do int.parse initializing
 Console.WriteLine("Where do you want to put your mark?");
+// gather the users choice
 choice = int.Parse(Console.ReadLine());
 // if else statement to check the place in the array
 if (PlayerArray[choice] != 'X' && PlayerArray[choice] != 'O') ;
@@ -32,11 +34,13 @@ if (PlayerArray[choice] != 'X' && PlayerArray[choice] != 'O') ;
     if (Player % 2 == 0)
     {
         PlayerArray[choice] = 'O';
+        Player++;
     }
     else
     {
         PlayerArray[choice] = 'X';
+        Player++;
     }
 }
 tk.CheckWinner(PlayerArray);
-// check the position in the array make sure they didn't just put O and X
+// print who the winner is and end the game
