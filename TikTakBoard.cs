f18782b01f6fb6d7cbb93aae9976a68ca00bddb2
fieldingdemars <fieldingdemars@FIELDINGDEMEF01>
2024-01-31 10:41:26 -0700

    beginning methods

--- TikTakBoard.cs
+++ TikTakBoard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,16 @@ namespace Mission04___Team1_11
 {
     internal class TikTakBoard
     {
+
+        public void printBoard() // method that prints the board based on the information passed to it
+        {
+          
+        }
+
+        public void checkWinner() // method that receives the game board array as input and returns if there is a winner and who it was
+        {
+
+        }
 
     }
 }
