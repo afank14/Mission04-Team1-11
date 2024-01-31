82afed246601ee3d694f27aa661be9ee8c43ef9c
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-01-31 10:19:28 -0700

    comments

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // hey I'm writing this comment -Eliza
 using System.Runtime.CompilerServices;
 
-string[] PlayerChoices = new string[9];
+char[] PlayerArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 int Player = 1;
 int choice;
 int start = 0;
@@ -16,5 +16,6 @@ else
 {
     Console.WriteLine("Player 1's turn!");
 }
-
 PrintBoard();
+// read line to see what the user wants to do int.parse initializing
+// if else statement to check the place in the array
