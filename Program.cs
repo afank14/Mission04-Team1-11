94209d3086694e73f098b7ebac865af0e24e0203
alexfankhauser <alexfankhauser@ALEXFANKHAU8EDB>
2024-01-31 10:39:31 -0700

    added do while looop

--- Program.cs
+++ Program.cs
@@ -9,34 +9,40 @@ char[] PlayerArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 int Player = 1;
 int choice;
 int start = 0;
-Console.WriteLine("PLayer1:X and Player2:O");
-if (Player % 2 == 0)
-{
-<<<<<<< HEAD
-    // testing
-}
-=======
-    Console.WriteLine("Player 2's turn!");
-}
-else
-{
-    Console.WriteLine("Player 1's turn!");
-}
-tk.PrintBoard();
-// read line to see what the user wants to do int.parse initializing
-Console.WriteLine("Where do you want to put your mark?");
-choice = int.Parse(Console.ReadLine());
-// if else statement to check the place in the array
-if (PlayerArray[choice] != 'X' && PlayerArray[choice] != 'O') ;
-{
+
+do { 
+    Console.WriteLine("PLayer1:X and Player2:O");
     if (Player % 2 == 0)
     {
-        PlayerArray[choice] = 'O';
+        Console.WriteLine("Player 2's turn!");
     }
     else
     {
-        PlayerArray[choice] = 'X';
+        Console.WriteLine("Player 1's turn!");
     }
-}
+    tk.PrintBoard();
+    // read line to see what the user wants to do int.parse initializing
+    Console.WriteLine("Where do you want to put your mark?");
+    choice = int.Parse(Console.ReadLine());
+    // if else statement to check the place in the array
+    if (PlayerArray[choice] != 'X' && PlayerArray[choice] != 'O') ;
+    {
+        if (Player % 2 == 0)
+        {
+            PlayerArray[choice] = 'O';
+        }
+        else
+        {
+            PlayerArray[choice] = 'X';
+        }
+    }
+
+    string result = tk.CheckWinner(PlayerArray)[0];
+    if (result == True)
+    {
+        start = 1;
+    }
+    Player++;
+} while (start == 0) ;
 tk.CheckWinner(PlayerArray);
 // check the position in the array make sure they didn't just put O and X
